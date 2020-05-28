7764b69a1f4c71ddf5c4a51a38d8cc70974039ba
Michael Bloem <mbloem@mosaicdatascience.com>
2020-05-28 05:34:33 -0400

    Adjusting to use sklearn logistic classifier.

--- src/kedro_mlflow_demo/pipelines/data_engineering/nodes.py
+++ src/kedro_mlflow_demo/pipelines/data_engineering/nodes.py
@@ -50,10 +50,6 @@ def split_data(data: pd.DataFrame, example_test_data_ratio: float) -> Dict[str, 
         "petal_width",
         "target",
     ]
-    classes = sorted(data["target"].unique())
-    # One-hot encoding for the target variable
-    data = pd.get_dummies(data, columns=["target"], prefix="", prefix_sep="")
-
     # Shuffle all the data
     data = data.sample(frac=1).reset_index(drop=True)
 
@@ -65,9 +61,9 @@ def split_data(data: pd.DataFrame, example_test_data_ratio: float) -> Dict[str, 
 
     # Split the data to features and labels
     train_data_x = training_data.loc[:, "sepal_length":"petal_width"]
-    train_data_y = training_data[classes]
+    train_data_y = training_data["target"]
     test_data_x = test_data.loc[:, "sepal_length":"petal_width"]
-    test_data_y = test_data[classes]
+    test_data_y = test_data["target"]
 
     # When returning many variables, it is a good practice to give them names:
     return dict(
--- src/kedro_mlflow_demo/pipelines/data_science/nodes.py
+++ src/kedro_mlflow_demo/pipelines/data_science/nodes.py
@@ -42,78 +42,55 @@ import mlflow
 from mlflow import sklearn as mlflow_sklearn
 from datetime import datetime
 from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.base import BaseEstimator
 
 
 def train_model(
-    train_x: pd.DataFrame, train_y: pd.DataFrame, parameters: Dict[str, Any]
-) -> np.ndarray:
+    train_x: pd.DataFrame,
+    train_y: pd.DataFrame,
+    parameters: Dict[str, Any]
+) -> BaseEstimator:
     """Node for training a simple multi-class logistic regression model. The
     number of training iterations as well as the learning rate are taken from
     conf/project/parameters.yml. All of the data as well as the parameters
     will be provided to this function at the time of execution.
     """
-    num_iter = parameters["example_num_train_iter"]
-    lr = parameters["example_learning_rate"]
-    X = train_x.to_numpy()
-    Y = train_y.to_numpy()
+    # Build a multi-class logistic regression model
+    model_params = parameters['model_params']
+    model = LogisticRegression(**model_params)
+    # Fit
+    model.fit(train_x, train_y)
 
-    # Add bias to the features
-    bias = np.ones((X.shape[0], 1))
-    X = np.concatenate((bias, X), axis=1)
-
-    weights = []
-    # Train one model for each class in Y
-    for k in range(Y.shape[1]):
-        # Initialise weights
-        theta = np.zeros(X.shape[1])
-        y = Y[:, k]
-        for _ in range(num_iter):
-            z = np.dot(X, theta)
-            h = _sigmoid(z)
-            gradient = np.dot(X.T, (h - y)) / y.size
-            theta -= lr * gradient
-        # Save the weights for each model
-        weights.append(theta)
-
-    # Return a joint multi-class model with weights for all classes
-    model = np.vstack(weights).transpose()
     mlflow_sklearn.log_model(sk_model=model, artifact_path="model")
-    mlflow.log_params(parameters)
+    mlflow.log_params(model_params)
+
     return model
 
 
-def predict(model: np.ndarray, test_x: pd.DataFrame) -> np.ndarray:
+def predict(
+    model: BaseEstimator, 
+    test_x: pd.DataFrame
+) -> pd.DataFrame:
     """Node for making predictions given a pre-trained model and a test set.
     """
-    X = test_x.to_numpy()
-
-    # Add bias to the features
-    bias = np.ones((X.shape[0], 1))
-    X = np.concatenate((bias, X), axis=1)
-
-    # Predict "probabilities" for each class
-    result = _sigmoid(np.dot(X, model))
-
-    # Return the index of the class with max probability for all samples
-    return np.argmax(result, axis=1)
+    # Return predictions
+    return model.predict(test_x)
 
 
-def report_accuracy(predictions: np.ndarray, test_y: pd.DataFrame) -> None:
+def report_accuracy(
+    predictions: pd.DataFrame, 
+    test_y: pd.DataFrame
+) -> None:
     """Node for reporting the accuracy of the predictions performed by the
     previous node. Notice that this function has no outputs, except logging.
     """
-    # Get true class index
-    target = np.argmax(test_y.to_numpy(), axis=1)
     # Calculate accuracy of predictions
-    accuracy = np.sum(predictions == target) / target.shape[0]
+    accuracy = (predictions == test_y).mean()
+    
     # Log the accuracy of the model
     log = logging.getLogger(__name__)
     log.info("Model accuracy on test set: %0.2f%%", accuracy * 100)
 
     mlflow.log_metric("accuracy", accuracy)
     mlflow.set_tag("Model Version", 1)
-
-
-def _sigmoid(z):
-    """A helper sigmoid function used by the training and the scoring nodes."""
-    return 1 / (1 + np.exp(-z))
